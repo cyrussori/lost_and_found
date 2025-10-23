8d4d9620cbedc1b78a8ef882daa9914ad7588aa3
cyrussori <soribellocj@gmail.com>
2025-10-23 15:19:25 -0700

    Quasi app

---
+++ lost_and_found/src/components/Postbar.jsx
@@ -0,0 +1,11 @@
+import "../css/main.css"
+
+export default function PostBar() {
+  return (
+    <>
+    <div className="postbar">
+      <button type="button">Post</button>
+    </div>
+    </>
+  )
+}
