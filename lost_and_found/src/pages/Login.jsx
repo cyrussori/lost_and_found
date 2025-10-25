381e90d805ebc84f8783acfc05cc32b3bbf72822
Cyrus Soribello <soribellocj@gmail.com>
2025-10-24 20:41:57 -0700

    Tailwind setup

--- lost_and_found/src/pages/Login.jsx
+++ lost_and_found/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import "../css/login.css";
 export default function Login() {
   return (
     <>
-      <div className="randClassName">test</div>
+      <div className="text-3xl font-bold text-blue-600">TEST</div>
     </>
   );
 }
