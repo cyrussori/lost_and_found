a154a80bac7571cb3d10059d557eebc4b568277e
cyrussori <soribellocj@gmail.com>
2025-10-26 01:02:03 -0700

    Added login page and signup page

--- lost_and_found/src/App.jsx
+++ lost_and_found/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Welcome from "./components/Welcome";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup/>} />
       </Routes>
     </Router>
   );
--- lost_and_found/src/pages/Signup.jsx
+++ lost_and_found/src/pages/Signup.jsx
@@ -1,19 +1,20 @@
-import "../css/login.css";
 import { Link } from "react-router-dom";
-export default function Login() {
+export default function Signup() {
   return (
     <>
       {/*Header*/}
       <div className="loginCard">
         <h1>Lost & Found</h1>
+        <p>Sign up to continue</p>
         <form className="loginForm" action="">
           <label for="email">Email</label>
           <input type="text" id="email" name="email" placeholder="Enter your email"></input>
+          <label for="fullName">Full name</label>
+          <input type="text" id="fullName" name="fullName" placeholder="Enter full name"></input>
           <label for="password">Password</label>
-          <input type="password" id="password" name="password" placeholder="Enter password"></input>
+          <input type="password" id="password" name="password" placeholder="Enter your password"></input>
           <input className="loginSubmit" type="submit" value="Continue"></input>
         </form>
-        <Link className="signupLink" to="/signup">Create an account</Link>
       </div>
     </>
   );
