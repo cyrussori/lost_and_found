ffb7abcf59d6b596cb4709462bb9a7d7bb9e5ae8
Molly <mollyang@g.ucla.edu>
2025-11-14 13:53:00 -0800

    Implemented the navbar page

--- lost_and_found/frontend/src/pages/Browse.jsx
+++ lost_and_found/frontend/src/pages/Browse.jsx
@@ -1,14 +1,10 @@
 import "../css/browse.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import CardPost from "../components/CardPost";
 import { getPosts } from "../services/api.js";
 
-// TODO: Context, for global posts.
-// All onclick => show all posts
-// Lost onclick => Show lost posts
-// Found onclick => Show found posts
 export default function Browse() {
   const [feed, setFeed] = useState([]);
   const [filterType, setFilterType] = useState("all");
@@ -29,16 +25,20 @@ export default function Browse() {
   return (
     <>
       <Navbar />
+
       {/*Header*/}
       <div className="browseHWrapper">
         <div className="browseHeader">
-          <button>All</button>
-          <button>Lost</button>
-          <button>Found</button>
+          <button onClick={() => setFilterType("all")}>All</button>
+          <button onClick={() => setFilterType("lost")}>Lost</button>
+          <button onClick={() => setFilterType("found")}>Found</button>
         </div>
       </div>
+
       <div className="browseWrapper">
-        <div className="browseCard"></div>
+        {filteredFeed.map((post) => (
+          <CardPost key={post._id} post={post} />
+        ))}
       </div>
     </>
   );
