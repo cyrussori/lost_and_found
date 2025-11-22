a0aeaed6ceaa1fa057ad0832180a8fbe3d0246f3
Cyrus Soribello <soribellocj@gmail.com>
2025-11-22 15:58:37 -0800

    Linux support

--- lost_and_found/backend/db/connection.js
+++ lost_and_found/backend/db/connection.js
@@ -1,13 +1,14 @@
 // This file is a connection between node.js server and MYSQL database
+import 'dotenv/config'
 import mysql from "mysql2";
 import dotenv from "dotenv";
 dotenv.config();
 
 //create a connection to the database we created in MYSQL
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  host: process.env.DB_HOST || '127.0.0.1',
+  user: process.env.DB_USER || 3306,
+  password: process.env.DB_PASS ?? process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 
