64f27b0df565e922c6bd5316316251ea8b4d3f65
cyrussori <soribellocj@gmail.com>
2025-11-25 17:14:57 -0800

    Add function to get posts for current user.

--- lost_and_found/frontend/src/services/api.js
+++ lost_and_found/frontend/src/services/api.js
@@ -54,6 +54,20 @@ export async function getPosts() {
   }
 }
 
+export async function getMyPosts() {
+    try {
+        const response = await fetch(`${API_BASE}/posts/me`, {
+            method: "GET",
+            credentials: "include",
+        });
+        if (!response.ok) throw new Error(`HTTP error; status: ${response.status}`);
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching posts', error);
+        return null;
+    }
+}
+
 export async function createPost(postData) {
     try {
         const response = await fetch(`${API_BASE}/posts`, {
