28e645e4ef02ffd388101d0aa63080e25a660a40
Shumin Yan <syan123@ucla.edu>
2025-11-27 21:11:12 -0800

    changed the frontpage to make the structure clearer

--- lost_and_found/frontend/src/components/Welcome.jsx
+++ lost_and_found/frontend/src/components/Welcome.jsx
@@ -1,29 +1,12 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
-import CardPost from "../components/CardPost";
-import Card from "../components/Card";
 import "../css/main.css";
 
 export default function Welcome() {
-  const [feed, setFeed] = useState([]);
-  const [card, setCard] = useState(false);
-
-  function handlePost() {
-    setCard(true);
-  }
-
-  function handleReport(post) {
-    setFeed((prev) => [post, ...prev]);
-    setCard(false);
-  }
-
   return (
     <>
       <div className="wrapper">
         <h1>LOGO</h1>
         <div className="linkBar">
-          <button className="btnStyle"> Lost </button>
-          <button className="btnStyle"> Found </button>
           <Link to="/browse">
             <button>Browse Posts</button>
           </Link>
@@ -42,19 +25,8 @@ export default function Welcome() {
         </div>
         <div>
           <div className="imagetemp">temp for img</div>
-          <button className="btnStyle" onClick={handlePost}>
-            Post
-          </button>
         </div>
       </div>
-
-      <div className="feed">
-        {feed.map((item) => (
-          <CardPost key={item.id} post={item} />
-        ))}
-      </div>
-
-      {card && <Card onClose={() => setCard(false)} onReport={handleReport} />}
     </>
   );
 }
