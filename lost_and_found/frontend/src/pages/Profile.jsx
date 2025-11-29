8ca6de273569c886713f2f02d8600bc253b9cf9b
cyrussori <soribellocj@gmail.com>
2025-11-28 23:31:42 -0800

    Remove IDK

--- lost_and_found/frontend/src/pages/Profile.jsx
+++ lost_and_found/frontend/src/pages/Profile.jsx
@@ -136,12 +136,10 @@ export default function Profile({ posts, setPosts }) {
               <button className="editProfileBtn">Edit profile</button>
               <div className="colWrapper">
                 <div className="colBtns">
-                  <button className={currTab === "posts" ? "currTab" : "" }
+                  <button className={currTab === "posts" ? "tab active" : "tab" }
                   onClick={() => setCurrTab("posts")}>Posts</button>
-                  <button className={currTab === "replies" ? "replies" : "" }
+                  <button className={currTab === "replies" ? "tab active" : "tab" }
                   onClick={() => setCurrTab("replies")}>Replies</button>
-                  <button className={currTab === "temp" ? "temp" : "" }
-                  onClick={() => setCurrTab("temp")}>IDK</button>
                 </div>
               </div>
             </div>
